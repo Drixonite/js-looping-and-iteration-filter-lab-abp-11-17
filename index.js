e4dd0c78c132497af793a799fe4374225791444f
David Kim <mail.ddk@gmail.com>
2017-11-14 23:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  return list.filter(function (driverName) {
+  return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase()}
   );
 }
